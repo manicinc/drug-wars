8dbe477ce2994217e781b87d98ae556aedc66b1e
Victor Evogor <victorevogor0001@gmail.com>
2025-07-02 14:08:49 +0100

    remove placeholder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.manic.drugwars',
+  appId: 'com.manicinc.drugwars',
   appName: 'Drug Wars',
   webDir: 'www',
   server: {
